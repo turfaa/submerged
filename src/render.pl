5a7553bc63203c88f51487dff7f8fc991ae7b0cf
Jauhar Arifin <jauhararifin10@gmail.com>
2016-11-24 17:31:27 +0700

    add proces logic

--- src/render.pl
+++ src/render.pl
@@ -1,12 +1,15 @@
 render_gameState :-
 	gameState(IsPowerOn, OxygenLevel, CurrentRoom, Inventory, Objects),
 	displayCurrentRoom(CurrentRoom),
+	displayStoryRoom(CurrentRoom),
 	displayInventory(Inventory),
 	displayOxygenLevel(OxygenLevel),
 	displayIsPowerOn(IsPowerOn),
 	displayObjects(Objects).
 
 displayCurrentRoom(CurrentRoom) :- write('You are in the '), write(CurrentRoom), write('.'), nl.
+
+displayStoryRoom(_).
 
 displayInventory([]) :- write('You are carrying nothing.'), nl.
 
